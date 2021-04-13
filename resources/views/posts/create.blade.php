079720fc42997c89eeac701162e7c36c88df09fc
KeiyaMorii <kei8.m.0123@gmail.com>
2021-04-13 23:06:21 +0900

    fix posts layouts

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,10 +3,21 @@
 @section('title', 'Simple Board')
 
 @section('content')
+
+    <h1>New Post</h1>
+
     <form action="/posts" method="POST">
     {{ csrf_field() }}
-    <input type="text" name="title">
-    <input type="text" name="content">
-    <input type="submit">
+    <div class="form-group">
+            <label for="exampleInputEmail1">Title</label>
+            <input type="text" name="title" class="form-control" aria-describedby="emailHelp">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Content</label>
+            <textarea name="content" class="form-control" ></textarea>
+        </div>
+        <button type="submit" class="btn btn-outline-primary">Submit</button>
     </form>
+
+    <a href="/posts">Back</a>
 @endsection
